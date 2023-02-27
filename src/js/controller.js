9cb1e345001ae293b39d954de8e5c7cc5830b647
hanzlasaadi <hanzalasaadi@gmail.com>
2023-02-27 10:32:44 +0500

    recipe loaded from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,41 @@
+import icons from '../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+// import { async } from 'regenerator-runtime';
+
 const recipeContainer = document.querySelector('.recipe');
+
+const getRecipe = async function () {
+  try {
+    // 1. Loading Recipe
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    showSpinner(recipeContainer);
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+    let { recipe } = data.data;
+    recipe = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      image: recipe.image_url,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (error) {
+    // console.error(error);
+    alert(error);
+  }
+};
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
